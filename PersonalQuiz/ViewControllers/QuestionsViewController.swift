ffbbfa6ae045197c9f1c8feb971b4926c1a43d8f
Nik <nik.alekseev.spb@gmail.com>
2021-04-12 16:01:00 +0300

    add setupUI in ViewDidLoud

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -14,21 +14,71 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var singleStackView: UIStackView!
     @IBOutlet var singleButtons: [UIButton]!
     
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    private let questions = Question.getQuestion()
+    private let answerChoosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[qestionIndex].answers
+    }
+    private var qestionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
     }
-    */
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed(_ sender: Any) {
+    }
+    
+}
+
+extension QuestionsViewController{
+    private func setupUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden.toggle()
+        }
+        
+        let currentQestion = questions[qestionIndex]
+        questionLabel.text = currentQestion.title
+        
+        let totalPrograsse = Float(qestionIndex) / Float(questions.count)
+        questionProgressView.setProgress(totalPrograsse, animated: true)
+    
+        title = "Вопрос № \(qestionIndex + 1) из \(questions.count)"
+    
+        showCurrentAnswers(for: currentQestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType){
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: break
+        case .ranged: break
+        }
+    }
+    
+    func showSingleStackView(with answer: [Answer]){
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answer ) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
 
 }
